df1b6ed4c130eeb478018c1e784c882c82508e75
Flavio De Stefano <flavio.destefano@caffeinalab.com>
2014-06-09 13:00:33 +0200

    First comit

---
+++ README.md
@@ -0,0 +1,82 @@
+com.caffeinalab.titanium.siriwave
+=================================
+
+![image](http://f.cl.ly/items/2q0I101D2t0p0W1Y0215/SWave.gif)
+
+### [Live CodePen Example](http://cdpn.io/yfegd)
+
+## Installation
+
+#### Via Gittio
+
+```
+gittio install com.caffeinalab.titanium.siriwave
+```
+
+#### Via Github
+
+```
+git clone git@github.com:CaffeinaLab/com.caffeinalab.titanium.siriwave.git app/widgets/com.caffeinalab.titanium.siriwave
+```
+
+And add in your *config.json*, under `dependencies`:
+
+```
+"dependencies": {
+    "com.caffeinalab.titanium.siriwave": "*"
+}
+```
+
+## Usage
+
+### Require in the View
+
+```xml
+<Widget src="com.caffeinalab.titanium.siriwave" id="siri" autostart="true" noise="1" color="#fff" />
+```
+
+And stylize globally via **app.tss*:
+
+```
+"#caffeinaSiriWave" : {
+	width: 320,
+	height: 150,
+	backgroundColor: 'black',
+	borderColor: 'red',
+	noise: 0.5
+}
+```
+
+### Options
+
+* **color**: The color of the wave
+* **autostart**: Autostart the wave?
+* **noise** The noise of the wave
+* **speed** The speed of the wave
+
+
+### Control in Controller
+
+### Set the noise
+
+```javascript
+$.siri.setNoise([ 0...1 ])
+```
+
+### Set the speed
+
+```javascript
+$.siri.setSpeed(0.4);
+```
+
+### Start the animation
+
+```javascript
+$.siri.start();
+```
+
+### Stop the animation
+
+```javascript
+$.siri.stop();
+```
---
+++ controllers/widget.js
@@ -0,0 +1,66 @@
+var args = _.extend({
+	width: 320,
+	height: 150,
+	color: '#fff',
+	noise: 1,
+	speed: 0.2,
+	autostart: true
+}, arguments[0] || {});
+
+
+function init() {
+	var html = '<!DOCTYPE html><html><head>';
+	html += '<meta name="viewport" content="width=device-width, user-scalable=no,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0" />';
+	html += '<style>html, body { margin: 0; padding: 0; }</style>';
+	html += '<script src="'+WPATH("SiriWaveJS/siriwave.js")+'"></script>';
+	html += '</head><body><script>';
+
+	html += 'var SW = new SiriWave(' + JSON.stringify({
+		width: args.width,
+		height: args.height,
+		color: args.color,
+		noise: args.noise,
+		speed: args.speed,
+		ratio: (function(){
+			if (OS_ANDROID) return Ti.Platform.displayCaps.logicalDensityFactor;
+			return Titanium.Platform.displayCaps.dpi/160;
+		})(),
+	}) + ');';
+
+	if (args.autostart) html += 'SW.start();';
+
+	html += '</script></body></html>';
+
+	$.caffeinaSiriWave.applyProperties(args);
+	$.caffeinaSiriWave.html = html;
+}
+
+
+function call(fn, ar) {
+	var ev = 'SW.' + fn + '(' + ( _.isObject(ar) ? JSON.stringify(ar) : ( ar!==undefined ? ar : '' ) ) + ');';
+	$.caffeinaSiriWave.evalJS(ev);
+}
+exports.call = call;
+
+exports.start = function() {
+	return call('start');
+};
+
+exports.start = function() {
+	return call('stop');
+};
+
+exports.setNoise = function(v) {
+	return call('setNoise', v);
+};
+
+exports.setSpeed = function(v) {
+	return call('setSpeed', v);
+};
+
+exports.set = function(noise, speed) {
+	return call('set', noise+','+speed);
+};
+
+
+init();
