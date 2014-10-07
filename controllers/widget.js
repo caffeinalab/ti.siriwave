262b1179b972ec92ea3c2b80bc8f66da3d825623
Flavio De Stefano <flavio.destefano@caffeinalab.com>
2014-10-07 10:17:46 +0200

    New version to fix new version
    Signed-off-by: Flavio De Stefano <flavio.destefano@caffeinalab.com>

--- controllers/widget.js
+++ controllers/widget.js
@@ -1,46 +1,30 @@
 var args = _.extend({
 	width: 320,
 	height: 150,
-	color: '#fff',
-	noise: 1,
-	speed: 0.2,
-	autostart: true
-}, arguments[0] || {});
-
+	ratio: OS_ANDROID ? Ti.Platform.displayCaps.logicalDensityFactor : Ti.Platform.displayCaps.dpi/160
+}, arguments[0]);
 
 function init() {
-	var html = '<!DOCTYPE html><html><head>';
+	var html = '<!DOCTYPE html>';
+	html += '<html><head>';
 	html += '<meta name="viewport" content="width=device-width, user-scalable=no,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0" />';
 	html += '<style>html,body{margin:0;padding:0;}</style>';
+	html += '</head><body>';
 	html += '<script src="'+WPATH("/SiriWaveJS/siriwave.js")+'"></script>';
-	html += '</head><body><script>';
-
-	html += 'var SW = new SiriWave(' + JSON.stringify({
-		width: args.width,
-		height: args.height,
-		color: args.color,
-		noise: args.noise,
-		speed: args.speed,
-		ratio: (function(){
-			if (OS_ANDROID) return Ti.Platform.displayCaps.logicalDensityFactor;
-			return Titanium.Platform.displayCaps.dpi/160;
-		})(),
-	}) + ');';
-
-	if (args.autostart) html += 'SW.start();';
-
-	html += '</script></body></html>';
-
-	$.caffeinaSiriWave.applyProperties(args);
-	$.caffeinaSiriWave.html = html;
+	html += '<script>window.SW = new SiriWave(' + JSON.stringify(args) + ');</script>';
+	html += '</body></html>';
+	$.siriWave.html = html;
 }
 
-
-function call(fn, ar) {
-	var ev = 'SW.' + fn + '(' + ( _.isObject(ar) ? JSON.stringify(ar) : ( ar!==undefined ? ar : '' ) ) + ');';
-	$.caffeinaSiriWave.evalJS(ev);
+function call(fn, val) {
+	$.siriWave.evalJS('window.SW.' + fn + '(' + (val == null ? '' : JSON.stringify(val)) + ');');
 }
 exports.call = call;
+
+function set(prop, val) {
+	$.siriWave.evalJS('window.SW.' + prop + '=' + JSON.stringify(val));
+}
+exports.set = set;
 
 exports.start = function() {
 	return call('start');
@@ -49,18 +33,5 @@ exports.start = function() {
 exports.start = function() {
 	return call('stop');
 };
-
-exports.setNoise = function(v) {
-	return call('setNoise', v);
-};
-
-exports.setSpeed = function(v) {
-	return call('setSpeed', v);
-};
-
-exports.set = function(noise, speed) {
-	return call('set', noise+','+speed);
-};
-
 
 init();
